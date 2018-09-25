1e47e9d288af28d784891ea21c53210cd75cb852
André Cruz <andremiguelcruz@msn.com>
2018-09-25 16:21:23 +0100

    chore: improve performance by caching loads to package.json

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
+const loadJsonFile = require('load-json-file');
 const requireDirectory = require('require-directory');
 const detectors = requireDirectory(module, './lib', { recurse: false });
+
+function createLoadPackageJson(dir) {
+    let packageJsonPromise;
+
+    return () => {
+        if (!packageJsonPromise) {
+            packageJsonPromise = loadJsonFile(path.join(dir, 'package.json'))
+            .catch((err) => {
+                if (err.code === 'ENOENT') {
+                    return null;
+                }
+
+                throw err;
+            });
+        }
+
+        return packageJsonPromise;
+    };
+}
 
 function detectRepoLinters(dir) {
     // Check if dir exists
@@ -17,9 +38,13 @@ function detectRepoLinters(dir) {
     })
     // Run the linter detectors and build the results
     .then(() => {
-        return Promise.all(Object.keys(detectors)
-            .map((name) => detectors[name](dir)
-                .then((detected) => detected && name)));
+        const loadPackageJson = createLoadPackageJson(dir);
+
+        return Promise.all(
+            Object.keys(detectors)
+            .map((name) => detectors[name](dir, loadPackageJson)
+            .then((detected) => detected && name))
+        );
     })
     .then((linters) => linters.filter((linter) => linter));
 }
